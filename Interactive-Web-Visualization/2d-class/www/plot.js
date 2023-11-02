9a85caebb6f7b4b669f487942764ed34b0948382
Evgeniia Komarova <102028836+aussiekom@users.noreply.github.com>
2023-11-02 07:33:58 +0100

    js scripts for more costom visualisations

---
+++ Interactive-Web-Visualization/2d-class/www/plot.js
@@ -0,0 +1,41 @@
+Shiny.addCustomMessageHandler("plotColors", function(colors) {
+    const dygraphsElement = document.getElementById("dygraph"); // R output ID
+if (dygraphsElement) {
+    const dygraphsWidget = window.HTMLWidgets.getInstance(dygraphsElement);
+    if (dygraphsWidget) {
+        const dygraphObject = dygraphsWidget.dygraph;
+        dygraphObject.updateOptions({ colors: colors });
+        }
+    }
+});    
+
+const drawHighlights = function(canvas, area, plot) {
+    if (plot.numColumns() > 1) {
+        canvas.fillStyle = "#FFFF00";
+    for( let row = 0; row < plot.numRows(); row++) {
+        
+        if (plot.getValue(row, 1) > plot.currentTemperatures[0]) {
+            //debugger;
+            const xValue = plot.getValue(row, 0);
+            const xValueBefore = plot.getValue(row === 0 ? 0 : row - 1, 0);
+            const left = plot.toDomXCoord(xValueBefore);
+            const right = plot.toDomXCoord(xValue);
+            canvas.fillRect(left, area.y, right - left, area.h);
+
+            }
+        }
+    }
+}
+
+const highlightTemperatures = function(canvas, area, plot){
+    if (!plot.currentTemperatures) {
+        getForecast().then(temperatures => {
+            plot.currentTemperatures = temperatures;
+            drawHighlights(canvas, area, plot);
+        }).catch(error => {
+            console.error(error);
+        });
+    } else {
+        drawHighlights(canvas, area, plot);
+    }
+};
