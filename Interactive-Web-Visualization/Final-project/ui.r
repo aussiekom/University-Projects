52eb737f691c77a1e041d0feb9964b33dfc067c1
Evgeniia Komarova <102028836+aussiekom@users.noreply.github.com>
2024-01-09 20:26:37 +0100

    map label functions to visual part

--- Interactive-Web-Visualization/Final-project/ui.r
+++ Interactive-Web-Visualization/Final-project/ui.r
@@ -4,6 +4,30 @@ library(shiny)
 
 source("model_prediction.r")
 
+# functions to generate the map labels
+get_weather_label <- function(city_name, weather_main) {
+  weather_label <- paste(sep = "",
+                         "<b><a href=''>",
+                         city_name,
+                         "</a></b>", "</br>",
+                         "<b>", weather_main, "</b></br>")
+  return(weather_label)
+}
+
+get_weather_detail_label <- function(result, city_name) {
+  weather_detail_label <- paste(sep = "",
+                                "<b><a href=''>",
+                                city_name,
+                                "</a></b>", "</br>",
+                                "<b>", result$weather[[1]]$main, "</b></br>",
+                                "Temperature: ", result$main$temp, " C </br>",
+                                "Visibility: ", result$visibility, " m </br>",
+                                "Humidity: ", result$main$humidity, " % </br>",
+                                "Wind Speed: ", result$wind$speed, " m/s </br>",
+                                "Datetime: ", result$dt_txt, " </br>")
+  return(weather_detail_label)
+}
+
 
 ### ui part --------
 ui <- shinyUI(
